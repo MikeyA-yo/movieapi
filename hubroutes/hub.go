5f6bc1e81fa5f379fc910f10aa522889dc11508f
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-06-29 11:57:59 +0100

    improved get title function

--- hubroutes/hub.go
+++ hubroutes/hub.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 )
 
-var Num = rand.Intn(26)
+var Num = rand.Intn(100)
 
 // Function to identify a series based on it's name, returns an array of bytes, takes a url and name as parameters
 func GetSeries(url string, name string) []byte {
@@ -36,7 +36,7 @@ func GetMovies(url string, name string) []byte {
 
 // function to Search for movies and series generally
 func GetSearch(url string, name string) []byte {
-	combineUrl := fmt.Sprintf("%v&s=%v", url, name)
+	combineUrl := fmt.Sprintf("%v&s=%v&page=%v", url, name, Num)
 	res, err := http.Get(combineUrl)
 	if err != nil {
 		fmt.Print("Error\n")
@@ -47,10 +47,10 @@ func GetSearch(url string, name string) []byte {
 }
 
 // Fuction to get random character
-func RandomCharacter() string {
-	aZ := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
-	return aZ[Num]
-}
+// func RandomCharacter() string {
+// 	aZ := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+// 	return aZ[Num]
+// }
 
 // function For a random word 178186
 func GetWord() string {
@@ -65,4 +65,50 @@ func GetWord() string {
 	body, _ := io.ReadAll(res.Body)
 	json.Unmarshal(body, &result)
 	return result[WordNum]
+}
+
+type serials struct {
+	Search []struct {
+		Title  string `json:"Title"`
+		Year   string `json:"Year"`
+		ImdbID string `json:"imdbID"`
+		Type   string `json:"Type"`
+		Poster string `json:"Poster"`
+	} `json:"Search"`
+	TotalResults string `json:"totalResults"`
+	Response     string `json:"Response"`
+}
+
+type searchResult struct {
+	Title string `json:"Title"`
+	Type  string `json:"Type"`
+}
+
+// actually get a title, and type to search based on GetWord() function
+func GetTitle() []searchResult {
+	var title serials
+	randomWord := GetWord()
+	URL := fmt.Sprintf("http://localhost:8080/search/%v", randomWord)
+	res, err := http.Get(URL)
+	if err != nil {
+		fmt.Print("Error\n")
+	}
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+	json.Unmarshal(body, &title)
+	if title.Response == "False" {
+		return GetTitle()
+	}
+	arrLength := len(title.Search)
+	titlesRes := []searchResult{}
+	//titles := []string{}
+	for i := 0; i < arrLength; i++ {
+		results := searchResult{
+			Title: title.Search[i].Title,
+			Type:  title.Search[i].Type,
+		}
+		titlesRes = append(titlesRes, results)
+		//titles = append(titles, title.Search[i].Title)
+	}
+	return titlesRes
 }
