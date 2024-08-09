417d12877eb61041b4e2412e627202664d976f4c
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-08-09 11:36:01 +0100

    well

--- docs/docs.go
+++ docs/docs.go
@@ -29,11 +29,11 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "movieapi-gcve.onrender.com",
 	BasePath:         "/",
 	Schemes:          []string{},
-	Title:            "MovieAPI",
-	Description:      "This is a sample server.",
+	Title:            "MovieHub",
+	Description:      "MovieHub API, get info's about movies",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
 	LeftDelim:        "{{",
